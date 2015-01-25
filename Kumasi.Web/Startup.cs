f7967f54f4a751c0b54d50171aa7b7bad96bcab8
Samuel Addai <addaisam@gmail.com>
2015-01-25 18:18:06 -0500

    This is a default code
    This is for the UI design

---
+++ Kumasi.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kumasi.Web.Startup))]
+namespace Kumasi.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
